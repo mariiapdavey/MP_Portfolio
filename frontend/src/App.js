256c72b27d97ad51dbb3b35d39c7ed4176859cb7
mariiapdavey <mariiapdavey@gmail.com>
2025-02-25 09:40:19 -0800

    added Homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import {Container} from 'react-bootstrap';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from './screens/HomeScreen';
 
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
 
     <main>
       <Container>
-        <h1>Welcome to my site!</h1>
+        <HomeScreen />
       </Container>
       
     </main>
